6bb79ad0e477dad685f3e3784adfcf82d376397b
Vojtěch Bartička <aitakaitov@gmail.com>
2022-11-06 20:44:01 +0100

    Refactor, WIP on async

--- node/python/app.py
+++ node/python/app.py
@@ -1,15 +1,13 @@
 import jsonpickle
 import random
-
 import os
-
 from flask_cors import CORS
 from flask import Flask, request
 from utils import *
-import threading
 import requests
 from datetime import datetime
 import argparse
+import threading
 
 import socket
 socket.setdefaulttimeout(5)
@@ -18,45 +16,43 @@ PRINT_TO_STD = True
 if not PRINT_TO_STD:
     open('output', 'w+', encoding='utf-8').close()
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--port', required=True, type=int)
-parser.add_argument('--n_nodes', required=True, type=int)
-args = vars(parser.parse_args())
-
-
+MODE = 'port'
 TIMEOUT_SEC = 30
-#IP_ADDRESS = os.environ['IP_ADDRESS']
-#NODE_COUNT = os.environ['NUM_NODES']
 
-PORT = args['port']
-IP_ADDRESS = 'localhost'
-NODE_COUNT = args['n_nodes']
+if MODE == 'port':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--port', required=True, type=int)
+    parser.add_argument('--n_nodes', required=True, type=int)
+    args = vars(parser.parse_args())
+    PORT = args['port']
+    IP_ADDRESS = 'localhost'
+    NODE_COUNT = args['n_nodes']
+elif MODE == 'ip':
+    IP_ADDRESS = os.environ['IP_ADDRESS']
+    NODE_COUNT = os.environ['NUM_NODES']
+    PORT = 5000
 
-if PORT != 5001:
-    network_info = NetworkInfo(random.randint(0, 2_000_000_000), IP_ADDRESS, NODE_COUNT, PORT)
-else:
-    network_info = NetworkInfo(2_000_000_000, IP_ADDRESS, NODE_COUNT, PORT)
 
-timers = {}
+#if PORT != 5001:
+network_info = NetworkInfo(random.randint(0, 2_000_000_000), IP_ADDRESS, NODE_COUNT, PORT, MODE)
+#else:
+#    network_info = NetworkInfo(2_000_000_000, IP_ADDRESS, NODE_COUNT, PORT)
+
+timer_manager = TimerManager(TIMEOUT_SEC)
 counters = {}
-
-
-def create_and_start_timer(func):
-    timer = threading.Timer(TIMEOUT_SEC, func)
-    timer.start()
-    return timer
 
 
 def send_leader_down_message():
     try:
         if network_info.leader_id != -1:
             return
-        log_message(f'Trying to send LEADER DOWN message to the right neighbour')
+        log_message(f'Sending LEADER_DOWN to the right neighbour')
         response = send_message(BaseRequest(network_info.id, MessageType.LEADER_DOWN))
-        log_message(f'Reached the right neighbour')
-        timers['leader_down'] = create_and_start_timer(send_leader_down_message)
+        log_message(f'OK sending LEADER_DOWN to the right neighbour')
     except BaseException:
-        timers['leader_down'] = create_and_start_timer(send_leader_down_message)
+        log_message('FAIL sending LEADER_DOWN to the right neighbour')
+
+    timer_manager.add_timer_and_run('leader_down', send_leader_down_message)
 
 
 def send_election_message():
@@ -66,26 +62,27 @@ def send_election_message():
             network_info.next_neighbour_shift()
             log_message(f'New right neighbour: {network_info.get_right_neighbour_address()}')
 
-        log_message(f'Trying to send an election message to the right neighbour')
+        log_message(f'Sending ELECTION to the right neighbour')
         response = send_message(BaseRequest(network_info.id, MessageType.ELECTION_ROUND))
         network_info.right_neighbour_id = response.json()['id']
-        log_message(f'Reached the right neighbour')
+        log_message(f'OK sending ELECTION to the right neighbour')
 
         # Now that we know our right neighbour is alive, we can start pinging him
-        timers['ping'] = create_and_start_timer(ping_right_neighbour)
+        exists = timer_manager.add_run_if_not_existing('ping', ping_right_neighbour)
+        if exists:
+            log_message('-- starting ping in send_election_message')
         counters['election_init'] = 0
 
         if not network_info.round_trip_made:
-            timers['election_init'] = create_and_start_timer(send_election_message)
+            timer_manager.add_timer_and_run('election_init', send_election_message)
+            log_message('-- round trip not made yet, continue sending ELECTION messages')
 
     except BaseException:
-        log_message(f'Could not contact the neighbour, retry in {TIMEOUT_SEC} seconds')
-        timers['election_init'] = create_and_start_timer(send_election_message)
+        log_message(f'FAIL sending ELECTION to the right neighbour')
+        timer_manager.add_timer_and_run('election_init', send_election_message)
+        log_message('-- continue sending ELECTION messages')
         counters['election_init'] += 1
 
-
-timers['election_init'] = create_and_start_timer(send_election_message)
-counters['election_init'] = 0
 
 def recover_neighbour_dead():
     leader_dead = network_info.right_neighbour_id == network_info.leader_id
@@ -104,6 +101,8 @@ def recover_neighbour_dead():
             response = send_message(BaseRequest(network_info.id, MessageType.PING))
             network_info.right_neighbour_id = response.json()['id']
             neighbour_reachable = True
+            timer_manager.add_timer_and_run('ping', ping_right_neighbour)
+            log_message('-- starting pinging after neighbour reached')
         except BaseException:
             log_message(f'Could not contact {network_info.right_neighbour_ip}')
 
@@ -112,35 +111,40 @@ def recover_neighbour_dead():
         # should be able to make it's way around the ring setting everyone's leader IDs to -1, allowing for the election
         # process to take place
         network_info.leader_id = -1
-        #send_message(BaseRequest(network_info.id, MessageType.LEADER_DOWN))
 
         # then we periodically send election messages - this is done in order to make sure that no one blocks the
         # election message because their leader ID is not -1 yet (possible with threaded servers)
-        #timers['election_init'] = create_and_start_timer(send_election_message)
-        timers['leader_down'] = create_and_start_timer(send_leader_down_message)
+        timer_manager.add_timer_and_run('leader_down', send_leader_down_message)
+        log_message('-- started leader down timer')
     else:
         # if the node is not the leader, we just send a message announcing it around and the leader catches it
         # he will then collect IDs again and color the ring
         if network_info.leader_id == network_info.id:
+            log_message('Sending NODE_DOWN message to SELF')
             send_message(BaseRequest(network_info.id, MessageType.NODE_DOWN), this_address=True)
         else:
+            log_message('Sending NODE_DOWN message to the right neighbour')
             send_message(BaseRequest(network_info.id, MessageType.NODE_DOWN))
-
-    if not timers['ping'].is_alive():
-        timers['ping'] = create_and_start_timer(ping_right_neighbour)
 
 
 def ping_right_neighbour():
     try:
-        log_message(f'Pinging right neighbour')
+        log_message(f'Pinging right neighbour {network_info.right_neighbour_ip}')
         send_message(BaseRequest(network_info.id, MessageType.PING))
-        log_message(f'Right neighbour responded')
+        log_message(f'Right neighbour responded to ping')
 
-        timers['ping'] = create_and_start_timer(ping_right_neighbour)
-
+        timer_manager.cancel_timer('ping')
+        timer_manager.add_timer_and_run('ping', ping_right_neighbour)
+        log_message('-- starting ping in ping_right_neighbour')
     except BaseException:
-        log_message(f'Could not contact the neighbour, assuming dead, attempting to recover')
+        timer_manager.cancel_timer('ping')
+        log_message(f'Could not ping the neighbour, assuming dead, attempting to recover')
         recover_neighbour_dead()
+
+
+def send_message_async(message, this_address=False):
+    thread = threading.Thread(target=send_message, args=(message, this_address))
+    thread.start()
 
 
 # --------------------------------------------------------------------------------------------------
@@ -160,7 +164,7 @@ def forward_message(message):
         message.sender_id = network_info.id
         send_message(message)
     except BaseException:
-        log_message(f'Could not forward message')
+        log_message(f'Could not forward message \n{message}')
 
 
 def sender_this_node(message):
@@ -180,12 +184,16 @@ def log_message(string):
 
 
 log_message(f'Node {network_info.id} starting up')
-#log_message(f'Node IP: {network_info.ip}\nNeighbour IP: {network_info.right_neighbour_ip}\nNumber of nodes: {network_info.node_count}')
 log_message(f'Node IP: {network_info.ip}:{network_info.port}\nNeighbour IP: {network_info.get_right_neighbour_address()}\nNumber of nodes: {network_info.node_count}')
+
+timer_manager.add_timer_and_run('election_init', send_election_message)
+log_message('-- starting ELECTION timer')
+counters['election_init'] = 0
+
 app = Flask(__name__)
 
-
-# TODO detect errors in message sending and initiate recovery
+import logging
+logging.getLogger('werkzeug').setLevel(logging.ERROR)
 
 
 @app.route('/message', methods=['POST'])
@@ -201,14 +209,21 @@ def process_message():
         network_info.leader_down = True
         # if we have not sent this message, just forward it and set leader ID to -1
         if not sender_this_node(data):
-            log_message('Received LEADER DOWN message, forwarding')
+            log_message('LEADER DOWN received, forwarding')
             network_info.leader_id = -1
             forward_message(data)
         # If we have sent this message, we stop sending them and we start sending election messages
         else:
-            if 'leader_down' in timers.keys():
-                timers['leader_down'].cancel()
-                timers['election_init'] = create_and_start_timer(send_election_message())
+            network_info.leader_down = False
+            log_message('LEADER DOWN received at origin')
+            log_message('-- cancelling leader down timer')
+            timer_manager.cancel_timer('leader_down')
+            log_message('-- starting election timer')
+            if timer_manager.check_timer_exists('leader_down'):
+                timer_manager.add_timer_and_run('election_init', send_election_message)
+            else:
+                log_message('-- leader down timer did not exist, election timer was not started')
+
         return '{ "id": ' + f'{network_info.id}' + '}', 200
 
     # a node in the ring went down
@@ -228,49 +243,62 @@ def process_message():
     elif data.message_type == MessageType.ELECTION_ROUND:
         # ignore if leader is already elected
         if network_info.leader_id != -1:
+            log_message('Ignoring ELECTION message as leader exists')
             return '{ "id": ' + f'{network_info.id}' + '}', 201
         # block messages with lower id
         if data.original_id < network_info.id:
             # if the leader is down, repeat the message
             if network_info.leader_down:
+                log_message('Received ELECTION message with lower ID, propagating my own as leader is down')
                 send_message(BaseRequest(network_info.id, MessageType.ELECTION_ROUND))
-            log_message(f'Received election message with lower ID, blocking')
+            else:
+                log_message(f'Received ELECTION message with lower ID, blocking')
             return '{ "id": ' + f'{network_info.id}' + '}', 200
         # this node is the leader
         elif sender_this_node(data):
             # this is here because of repeat messages
             if network_info.leader_id == -1:
                 # stop sending election messages
-                timers['election_init'].cancel()
+                log_message('-- cancelling election timer')
+                timer_manager.cancel_timer('election_init')
                 network_info.round_trip_made = True
                 # announce leader
-                log_message(f'Election message came back to origin, announcing as leader')
+                log_message(f'ELECTION message came back to origin, announcing as leader')
                 send_message(BaseRequest(network_info.id, MessageType.LEADER_ELECTED))
                 network_info.leader_id = network_info.id
 
             return '{ "id": ' + f'{network_info.id}' + '}', 200
         # forward the message
         else:
+            log_message('Forwarding ELECTION message')
             forward_message(data)
             return '{ "id": ' + f'{network_info.id}' + '}', 200
 
     # register the elected leader
     elif data.message_type == MessageType.LEADER_ELECTED:
+        log_message(f'LEADER_ELECTED message received')
         # leader is elected, we don't need to send election messages
-        timers['election_init'].cancel()
+        timer_manager.cancel_timer('election_init')
+        log_message('-- election timer stopped as leader was elected')
+
+        exists = timer_manager.add_run_if_not_existing('ping', ping_right_neighbour)
+        if not exists:
+            log_message('-- starting ping in leader_elected')
+
         network_info.round_trip_made = True
         network_info.leader_down = False
 
-        log_message(f'Leader elected message received')
+        log_message(f'Setting leader ID')
         network_info.leader_id = data.original_id
 
         # if the message returns back to the leader
         # send a message to collect IDs
         if sender_this_node(data):
-            log_message(f'Leader elected message came back to leader')
-            log_message(f'Sending collection message')
+            log_message(f'LEADER ELECTED message came back to leader')
+            log_message(f'Sending COLLECTION message')
             send_message(CollectRequest(network_info.id))
         else:
+            log_message('Forwarding LEADER_ELECTED message')
             forward_message(data)
 
         return '{ "id": ' + f'{network_info.id}' + '}', 200
@@ -279,16 +307,16 @@ def process_message():
     elif data.message_type == MessageType.COLLECT_IDS:
         # if the message came back
         if sender_this_node(data):
-            log_message(f'Collection message came back to origin')
+            log_message(f'COLLECTION message came back to origin')
             network_info.node_ids = data.ids
-            log_message(f'Color set to GREEN')
-            log_message(f'Sending coloring request')
+            log_message(f'Color set to Color.GREEN')
+            log_message(f'Sending COLORING request')
             color_request = ColorRequest(network_info.id, network_info.node_ids)
             send_message(color_request)
             pass
         # add node ID and pass message
         else:
-            log_message(f'Adding ID to the collection message')
+            log_message(f'Adding ID to the COLLECTION message')
             data.ids.append(network_info.id)
             send_message(data)
 
@@ -298,7 +326,7 @@ def process_message():
     elif data.message_type == MessageType.COLORING:
         # message came back
         if sender_this_node(data):
-            log_message('Coloring request came back to origin')
+            log_message('COLORING request came back to origin')
             log_message('Colors are all set')
             log_message(f'\nNode ID\tColor\n' + '\n'.join(f'{node}\t {color}' for node, color in data.node_color_dict.
                                                           items()))
@@ -312,5 +340,8 @@ def process_message():
 
 
 CORS(app)
-#app.run('0.0.0.0')
-app.run(host='localhost', port=PORT)
+
+if MODE == 'port':
+    app.run(host='localhost', port=PORT)
+elif MODE == 'ip':
+    app.run(host='0.0.0.0', port=PORT)
--- node/python/utils.py
+++ node/python/utils.py
@@ -1,4 +1,5 @@
 import math
+import threading
 from enum import Enum
 
 GREEN_COLOR_FRACTION = 1 / 3
@@ -6,7 +7,7 @@ IP_OFFSET = 100
 
 
 class NetworkInfo:
-    def __init__(self, _id, ip, node_count, port):
+    def __init__(self, _id, ip, node_count, port, mode):
         self.id = _id
         self.node_count = int(node_count)
         self.ip = ip
@@ -18,20 +19,25 @@ class NetworkInfo:
 
         self.round_trip_made = False
         self.leader_down = False
+        self.mode = mode
 
-        #split_ip = ip.split('.')
-        #self.ip_prefix = split_ip[0] + '.' + split_ip[1] + '.' + split_ip[2]
-        #this = int(split_ip[3])
+        if mode == 'ip':
+            split_ip = ip.split('.')
+            self.ip_prefix = split_ip[0] + '.' + split_ip[1] + '.' + split_ip[2]
+            this = int(split_ip[3])
 
-        # One to the right if we are not the last node, otherwise the first node
-        #if self.node_count == 2:
-        #    _next = 1 if this % 2 == 0 else 2
-        #else:
-        #    _next = this + 1 if this - IP_OFFSET != self.node_count else IP_OFFSET + 1
-        _next = port + 1 if port - 5000 != self.node_count else 5000 + 1
-        self.right_neighbour_ip = f'{self.ip}'
-        self.right_neighbour_port = _next
-        #self.right_neighbour_ip = f'{self.ip_prefix}.{_next}'
+            # One to the right if we are not the last node, otherwise the first node
+            if self.node_count == 2:
+                _next = 1 if this % 2 == 0 else 2
+            else:
+                _next = this + 1 if this - IP_OFFSET != self.node_count else IP_OFFSET + 1
+            self.right_neighbour_ip = f'{self.ip_prefix}.{_next}'
+            self.right_neighbour_port = port
+
+        elif mode == 'port':
+            _next = port + 1 if port - 5000 != self.node_count else 5000 + 1
+            self.right_neighbour_ip = f'{self.ip}'
+            self.right_neighbour_port = _next
 
         self.right_neighbour_id = -1
 
@@ -42,19 +48,64 @@ class NetworkInfo:
         return f'http://{self.ip}:{self.port}/message'
 
     def next_neighbour_shift(self):
-        #split_ip = self.right_neighbour_ip.split('.')
-        #neighbour = int(split_ip[3])
+        if self.mode == 'ip':
+            split_ip = self.right_neighbour_ip.split('.')
+            neighbour = int(split_ip[3])
 
-        #if self.node_count == 2:
-            # With two nodes we can't really shift anymore
-        #    return False
-        #else:
-        #    _next = neighbour + 1 if neighbour - IP_OFFSET != self.node_count else IP_OFFSET + 1
+            if self.node_count == 2:
+                # With two nodes we can't really shift anymore
+                return False
+            else:
+                _next = neighbour + 1 if neighbour - IP_OFFSET != self.node_count else IP_OFFSET + 1
 
-        #self.right_neighbour_ip = f'{self.ip_prefix}.{_next}'
+            self.right_neighbour_ip = f'{self.ip_prefix}.{_next}'
+            return self.right_neighbour_ip != self.ip
+        elif self.mode == 'port':
+            self.right_neighbour_port = self.right_neighbour_port + 1 if self.right_neighbour_port - 5000 != self.node_count else 5000 + 1
+            return self.right_neighbour_port != self.port
 
-        self.right_neighbour_port = self.right_neighbour_port + 1 if self.right_neighbour_port - 5000 != self.node_count else 5000 + 1
-        return self.right_neighbour_port != self.port
+
+class TimerManager:
+    def __init__(self, timeout):
+        self.timers = {}
+        self.timeout = timeout
+        self.lock = threading.RLock()
+
+    def add_timer_and_run(self, key, func):
+        self.lock.acquire()
+        if key in self.timers.keys():
+            if not self.timers[key].finished.is_set():
+                self.lock.release()
+                return
+            else:
+                print(f'adding {key} timer')
+                self.timers[key] = threading.Timer(self.timeout, func)
+                self.timers[key].start()
+        else:
+            print(f'adding {key} timer')
+            self.timers[key] = threading.Timer(self.timeout, func)
+            self.timers[key].start()
+        self.lock.release()
+
+    def cancel_timer(self, key):
+        self.lock.acquire()
+        if key in self.timers.keys():
+            self.timers[key].cancel()
+        self.lock.release()
+
+    def check_timer_exists(self, key):
+        self.lock.acquire()
+        exists = key in self.timers.keys()
+        self.lock.release()
+        return exists
+
+    def add_run_if_not_existing(self, key, func):
+        self.lock.acquire()
+        exists = self.check_timer_exists(key)
+        if not exists:
+            self.add_timer_and_run(key, func)
+        self.lock.release()
+        return exists
 
 
 class BaseRequest:
